126f5895d136f8bf345a50202c7837f421b132b6
Javi <javii.oteero@gmail.com>
2024-04-04 14:28:16 +0200

    Todos los tests pasados

--- src/es/iesmz/tests/TimeTest.java
+++ src/es/iesmz/tests/TimeTest.java
@@ -10,4 +10,32 @@ class TimeTest {
         Time expectedTime = new Time(12, 14, 16);
         assertEquals(expectedTime, time.nextSecond());
     }
+
+    @Test
+    public void testNextSecond2() {
+        Time time = new Time(12, 59, 44);
+        Time expectedTime = new Time(12, 59, 45);
+        assertEquals(expectedTime, time.nextSecond());
+    }
+
+    @Test
+    public void testNextSecond3() {
+        Time time = new Time(1, 9, 59);
+        Time expectedTime = new Time(1, 10, 0);
+        assertEquals(expectedTime, time.nextSecond());
+    }
+
+    @Test
+    public void testNextSecond4() {
+        Time time = new Time(17, 59, 59);
+        Time expectedTime = new Time(18, 0, 0);
+        assertEquals(expectedTime, time.nextSecond());
+    }
+
+    @Test
+    public void testNextSecond5() {
+        Time time = new Time(23, 59, 59);
+        Time expectedTime = new Time(0, 0, 0);
+        assertEquals(expectedTime, time.nextSecond());
+    }
 }
